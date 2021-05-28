06484d12fa260f6cdddd5f8504f742003b08b166
Gautam jain <55816462+gautamj-tech@users.noreply.github.com>
2021-05-28 19:51:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,17 +5,54 @@ const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const bodyparser = require('body-parser');
+const puppeteer=require('puppeteer');
+
 
 const usersController = require('./controllers/usersController');
 
 var app = express();
-app.use(bodyparser.urlencoded({
-    extended: true
-}));
-app.use(bodyparser.json());
-app.set('views', path.join(__dirname, '/views/'));
-app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
-app.set('view engine', 'hbs');
+(async () =>{
+    try{
+    const browser=await puppeteer.launch({headless:false});
+    const page=await browser.newPage();
+    await page.goto('https://www.instagram.com/accounts/login', { waitUntil: "networkidle2" });
+    await page.type('input[name=username]', 'username', { delay: 20 }); // in username add any instagram username
+    await page.type('input[name=password]', 'password', { delay: 20 }); // in password add any instagram password
+    await page.click('button[type=submit]', { delay: 20 });
+    await page.waitFor(5000);
+    await page.goto(`https://www.instagram.com/gautaamm`, { waitUntil: "networkidle2" });
+    "]');
+    await page.waitFor(2000);
+    
+    const imageURL=await page.$$(".Ckrof");
+    for(let i=0;i<imageURL.length;i++){
+    
+        const button=await page.$('.aoVrC');
+      
+       button.click();
+       await page.waitFor(2000);
+       const totalclicks=await page.$$('._7zQEa');
+      console.log('tl',totalclicks.length);
+       for(let j=0;j<totalclicks.length;j++){
+        let image=await page.$eval(".qbCDp img",img=>img.src);
+        console.log(image);
+        const button1=await page.$('.FhutL');
+        
+       button1.click();}
+       
+       
+
+       
+
+     
+
+    }
+    }
+    catch(e){
+        console.log('our error',e);
+    }
+})();
+
 
 app.listen(3000, () => {
     console.log('Express server started at port : 3000');
